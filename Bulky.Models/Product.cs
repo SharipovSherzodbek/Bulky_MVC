be3095ec1138225a9754c4106fda23cae650b7e3
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-11-10 13:02:41 +0500

    CRUD -> For Product

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +13,7 @@ namespace BulkyBook.Models
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string Description {  get; set; }
         [Required]
         public string ISBN { get; set; }
         [Required]
@@ -24,19 +23,17 @@ namespace BulkyBook.Models
         [Range(1, 1000)]
         public double ListPrice { get; set; }
 
-        [Required]
         [Display(Name = "Price for 1-50")]
         [Range(1, 1000)]
         public double Price { get; set; }
 
-        [Required]
         [Display(Name = "Price for 50+")]
         [Range(1, 1000)]
         public double Price50 { get; set; }
 
-        [Required]
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
     }
 }
