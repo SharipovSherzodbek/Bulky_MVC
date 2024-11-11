dc7a3034215e737a997bea22ed6eadbcf208cd75
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-11-11 23:40:14 +0500

    ADDED: Image URL

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -39,5 +39,7 @@ namespace BulkyBook.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
